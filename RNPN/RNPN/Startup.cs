d17578567517615a101c6ae7f3b91792c90f772c
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2021-10-18 09:43:26 -0600

    Animacion y mejoras estéticas
    Pagina del listado de personas

--- RNPN/RNPN/Startup.cs
+++ RNPN/RNPN/Startup.cs
@@ -59,7 +59,7 @@ namespace RNPN
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Persona}/{action=Index}/{id?}");
+                    pattern: "{controller=Persona}/{action=ListaPersonas}/{id?}");
             });
         }
     }
